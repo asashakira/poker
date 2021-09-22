dff80d4f6d6af3c3cc305aa82fb27bcd2fe8ae50
akira <akira340@users.noreply.github.com>
2021-09-22 13:41:59 +0900

    added react

--- db/init.sql
+++
@@ -1,3 +0,0 @@
-drop table user_info;
-create table user_info(name text, pass text, coin int);
-insert into user_info values('akira', 'pikmin', 10000);
--- texas.h
+++
@@ -1,5 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-extern int n_players;
-extern vector<int> bestHand(vector<int> h);
